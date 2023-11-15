dfac2cbb5d42bcbad4d43059784c660839af5fff
relliv <epsilonelittr@gmail.com>
2023-11-16 02:27:45 +0300

    Add tailwind support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+
+  server: {
+    port: 3000,
+  },
+
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+});
