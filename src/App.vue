c9d8333fe392a2fb2e0c7dff734e57383ebdb318
relliv <epsilonelittr@gmail.com>
2024-11-08 21:24:12 +0300

    Add absolute path

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue';
+  import HelloWorld from '@/components/HelloWorld.vue';
 </script>
 
 <style scoped lang="scss">
