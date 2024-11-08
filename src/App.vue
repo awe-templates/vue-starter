a46874f4cf2879a3899973354096a6710f9a1f8d
relliv <epsilonelittr@gmail.com>
2024-11-08 21:21:05 +0300

    Update eslint and prettier configs

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <main>
     <div class="logos">
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
 
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
     </div>
 
     <HelloWorld msg="Vite + Vue + TypeScript + Tailwind" />
@@ -15,11 +15,11 @@
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue"
+  import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <style scoped lang="scss">
-.logos {
+  .logos {
     @apply flex flex-row gap-5;
 
     .logo {
@@ -32,9 +32,10 @@ import HelloWorld from "./components/HelloWorld.vue"
       &:hover {
         filter: drop-shadow(0 0 2em #646cffaa);
       }
+
       &.vue:hover {
         filter: drop-shadow(0 0 2em #42b883aa);
       }
     }
-}
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,10 @@
-<script setup lang="ts">
-import { ref } from "vue"
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button class="border border-emerald-500 px-4 py-2" type="button" @click="count++">count is {{ count }}</button>
+    <button class="border border-emerald-500 px-4 py-2" type="button" @click="count++">
+      count is {{ count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -27,8 +21,23 @@ const count = ref(0)
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">
+    Click on the Vite and Vue logos to learn more
+  </p>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+})
+
+const count = ref(0)
+</script>
 
 <style scoped lang="scss">
 h1 {
