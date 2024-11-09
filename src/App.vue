539ebcd8c1499f09750648021fb36418e8b3c38f
relliv <epsilonelittr@gmail.com>
2024-11-10 01:23:33 +0300

    Update demo page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,53 @@
 <template>
   <main>
     <div class="logos">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <!-- Vite -->
+      <a href="https://vite.dev/" target="_blank">
+        <img
+          src="./assets/VscodeIconsFileTypeVite.svg"
+          class="logo vite"
+          alt="Vite logo"
+        />
       </a>
 
+      <!-- Vue -->
       <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        <img
+          src="./assets/VscodeIconsFileTypeVue.svg"
+          class="logo vue"
+          alt="Vue logo"
+        />
+      </a>
+
+      <!-- Typescript -->
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img
+          src="./assets/VscodeIconsFileTypeTypescriptOfficial.svg"
+          class="logo ts"
+          alt="TypeScript logo"
+        />
+      </a>
+
+      <!-- SASS -->
+      <a href="https://sass-lang.com/" target="_blank">
+        <img
+          src="./assets/VscodeIconsFileTypeSass.svg"
+          class="logo sass"
+          alt="SASS logo"
+        />
+      </a>
+
+      <!-- Tailwind -->
+      <a href="https://tailwindcss.com/" target="_blank">
+        <img
+          src="./assets/VscodeIconsFileTypeTailwind.svg"
+          class="logo tw"
+          alt="Tailwind logo"
+        />
       </a>
     </div>
 
-    <HelloWorld msg="Vite + Vue + TypeScript + Tailwind" />
+    <HelloWorld message="Vite + Vue + TypeScript + SASS + Tailwind" />
   </main>
 </template>
 
@@ -29,12 +66,24 @@
       will-change: filter;
       transition: filter 300ms;
 
-      &:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
+      &.vite:hover {
+        filter: drop-shadow(0 0 2em #bd34feaa);
       }
 
       &.vue:hover {
-        filter: drop-shadow(0 0 2em #42b883aa);
+        filter: drop-shadow(0 0 2em #41b883aa);
+      }
+
+      &.ts:hover {
+        filter: drop-shadow(0 0 2em #3178c6aa);
+      }
+
+      &.sass:hover {
+        filter: drop-shadow(0 0 2em #cd6799aa);
+      }
+
+      &.tw:hover {
+        filter: drop-shadow(0 0 2em #44a8b3aa);
       }
     }
   }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,22 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ message }}</h1>
 
   <div class="card">
     <button
-      class="border border-emerald-500 px-4 py-2"
+      class="border border-emerald-500 px-4 py-2 rounded-lg"
       type="button"
       @click="count++"
     >
       count is {{ count }}
     </button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code>
-      to test HMR
-    </p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">
-      create-vue
-    </a>
-    , the official Vue + Vite starter
-  </p>
-
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <script setup lang="ts">
-  import {ref} from 'vue';
+  import { ref } from 'vue';
 
   defineProps({
-    msg: {
+    message: {
       type: String,
       required: true,
     },
